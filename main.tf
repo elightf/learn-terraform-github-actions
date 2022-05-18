c6e8c60637f28155e6de14661562eb8a2c174e43
elightf <90856336+elightf@users.noreply.github.com>
2022-05-18 13:07:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "elightf"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "git-actions-demo"
     }
   }
 }
